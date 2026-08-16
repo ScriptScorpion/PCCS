99c60bc7b1ccd531b74aa81708ec0fc9cb28112c
ScriptScorpion <maksim.09.09@bk.ru>
2026-08-16 17:28:59 +0300

    Update

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # PCCS (Punch Card Creator Simulator)
-PCCS - is tool that simulates creation of the 1971 style punch cards for the [IBM 029](https://gist.github.com/Pozo/1004284).
+PCCS - is tool that simulates creation of the 1965-1971 style punch cards for the [IBM 024](https://dn721909.ca.archive.org/0/items/bitsavers_ibmpunched24052032426CardPunchReferenceManualOct19_6161673/A24-0520-3_24_26_Card_Punch_Reference_Manual_Oct1965.pdf).
 
 ## Requirements:
 * g++
--- main.cpp
+++ main.cpp
@@ -1,11 +1,26 @@
 #pragma GCC diagnostic ignored "-Wunused-parameter"
 #include <ncurses.h>
 #include <iostream>
-#include <unistd.h>
+#include <thread>
+#include <chrono>
 #include <cstring>
+#include <string>
+
+#define ROW0 6
+#define ROW1 8
+#define ROW2 10
+#define ROW3 12
+#define ROW4 14
+#define ROW5 16
+#define ROW6 18
+#define ROW7 20
+#define ROW8 22
+#define ROW9 24
+#define ROW11 4
+#define ROW12 2
 
 void draw_ascii(int x, int y) {
-    if (y < 26) { // not full card can be displayed 
+    if (y < 29) { // not full card can be displayed 
         return;
     }
 
@@ -25,15 +40,20 @@ void draw_ascii(int x, int y) {
     size_t col = 0;
     size_t rows = 0;
     
+    for (int i = 2; i < 5; i += 2) {
+        for (int j = 4; j < (x-4); j += 3) {
+            mvprintw(i, j, " ");
+        }
+    }
 
-    for (int i = 3; (i < (y-2)) && (col <= 9); i += 2) {
-        for (int j = 4; (j < (x-4)); j += 3) {
+    for (int i = 6; (i < (y-2)) && (col <= 9); i += 2) {
+        for (int j = 4; j < (x-4); j += 3) {
             mvprintw(i, j, "%li", col);   
         }
         col++;
     }
     for (int i = 4; i < (x-4); i += 3) {
-        mvprintw(24, i, "%li", rows);
+        mvprintw(27, i, "%li", rows);
         rows++;
     }
 }
@@ -61,108 +81,221 @@ void draw_ascii(int x, int y, size_t *ptr,  const char ch) {
 
     switch (ch) {
         case '0': {
-            mvprintw(3, 4+(*ptr)," ");
+            mvprintw(ROW0, 4+(*ptr)," ");
             break;
         }
         case '1': {
-            mvprintw(5, 4+(*ptr)," ");
+            mvprintw(ROW1, 4+(*ptr)," ");
             break;
         }
         case '2': {
-            mvprintw(7, 4+(*ptr)," ");
+            mvprintw(ROW2, 4+(*ptr)," ");
             break;
         }
         case '3': {
-            mvprintw(9, 4+(*ptr)," ");
+            mvprintw(ROW3, 4+(*ptr)," ");
             break;
         }
         case '4': {
-            mvprintw(11, 4+(*ptr)," ");
+            mvprintw(ROW4, 4+(*ptr)," ");
             break;
         }
         case '5': {
-            mvprintw(13, 4+(*ptr)," ");
+            mvprintw(ROW5, 4+(*ptr)," ");
             break;
         }
         case '6': {
-            mvprintw(15, 4+(*ptr)," ");
+            mvprintw(ROW6, 4+(*ptr)," ");
             break;
         }
         case '7': {
-            mvprintw(17, 4+(*ptr)," ");
+            mvprintw(ROW7, 4+(*ptr)," ");
             break;
         }
         case '8': {
-            mvprintw(19, 4+(*ptr)," ");
+            mvprintw(ROW8, 4+(*ptr)," ");
             break;
         }
         case '9': {
-            mvprintw(21, 4+(*ptr)," ");
+            mvprintw(ROW9, 4+(*ptr)," ");
             break;
         }
-        case '#': {
-            mvprintw(9, 4+(*ptr)," ");
-            mvprintw(19, 4+(*ptr)," ");
+        case 'A': {
+            mvprintw(ROW1, 4+(*ptr), " ");
+            mvprintw(ROW12, 4+(*ptr), "*");
             break;
         }
-        case '\"': {
-            mvprintw(17, 4+(*ptr)," ");
-            mvprintw(19, 4+(*ptr)," ");
+        case 'B': {
+            mvprintw(ROW2, 4+(*ptr), " ");
+            mvprintw(ROW12, 4+(*ptr), "*");
             break;
         }
-        case '=': {
-            mvprintw(15, 4+(*ptr)," ");
-            mvprintw(19, 4+(*ptr)," ");
+        case 'C': {
+            mvprintw(ROW3, 4+(*ptr), " ");
+            mvprintw(ROW12, 4+(*ptr), "*");
+            break;
+        }
+        case 'D': {
+            mvprintw(ROW4, 4+(*ptr), " ");
+            mvprintw(ROW12, 4+(*ptr), "*");
+            break;
+        }
+        case 'E': {
+            mvprintw(ROW5, 4+(*ptr), " ");
+            mvprintw(ROW12, 4+(*ptr), "*");
+            break;
+        }
+        case 'F': {
+            mvprintw(ROW6, 4+(*ptr), " ");
+            mvprintw(ROW12, 4+(*ptr), "*");
+            break;
+        }
+        case 'G': {
+            mvprintw(ROW7, 4+(*ptr), " ");
+            mvprintw(ROW12, 4+(*ptr), "*");
+            break;
+        }
+        case 'H': {
+            mvprintw(ROW8, 4+(*ptr), " ");
+            mvprintw(ROW12, 4+(*ptr), "*");
+            break;
+        }
+        case 'I': {
+            mvprintw(ROW9, 4+(*ptr), " ");
+            mvprintw(ROW12, 4+(*ptr), "*");
+            break;
+        }
+        case 'J': {
+            mvprintw(ROW1, 4+(*ptr), " ");
+            mvprintw(ROW11, 4+(*ptr), "*");
+            break;
+        }
+        case 'K': {
+            mvprintw(ROW2, 4+(*ptr), " ");
+            mvprintw(ROW11, 4+(*ptr), "*");
+            break;
+        }
+        case 'L': {
+            mvprintw(ROW3, 4+(*ptr), " ");
+            mvprintw(ROW11, 4+(*ptr), "*");
+            break;
+        }
+        case 'M': {
+            mvprintw(ROW4, 4+(*ptr), " ");
+            mvprintw(ROW11, 4+(*ptr), "*");
+            break;
+        }
+        case 'N': {
+            mvprintw(ROW5, 4+(*ptr), " ");
+            mvprintw(ROW11, 4+(*ptr), "*");
+            break;
+        }
+        case 'O': {
+            mvprintw(ROW6, 4+(*ptr), " ");
+            mvprintw(ROW11, 4+(*ptr), "*");
+            break;
+        }
+        case 'P': {
+            mvprintw(ROW7, 4+(*ptr), " ");
+            mvprintw(ROW11, 4+(*ptr), "*");
+            break;
+        }
+        case 'Q': {
+            mvprintw(ROW8, 4+(*ptr), " ");
+            mvprintw(ROW11, 4+(*ptr), "*");
+            break;
+        }
+        case 'R': {
+            mvprintw(ROW9, 4+(*ptr), " ");
+            mvprintw(ROW11, 4+(*ptr), "*");
+            break;
+        }
+        case 'S': {
+            mvprintw(ROW2, 4+(*ptr), " ");
+            mvprintw(ROW0, 4+(*ptr), " ");
+            break;
+        }
+        case 'T': {
+            mvprintw(ROW3, 4+(*ptr), " ");
+            mvprintw(ROW0, 4+(*ptr), " ");
+            break;
+        }
+        case 'U': {
+            mvprintw(ROW4, 4+(*ptr), " ");
+            mvprintw(ROW0, 4+(*ptr), " ");
+            break;
+        }
+        case 'V': {
+            mvprintw(ROW5, 4+(*ptr), " ");
+            mvprintw(ROW0, 4+(*ptr), " ");
+            break;
+        }
+        case 'W': {
+            mvprintw(ROW6, 4+(*ptr), " ");
+            mvprintw(ROW0, 4+(*ptr), " ");
+            break;
+        }
+        case 'X': {
+            mvprintw(ROW7, 4+(*ptr), " ");
+            mvprintw(ROW0, 4+(*ptr), " ");
+            break;
+        }
+        case 'Y': {
+            mvprintw(ROW8, 4+(*ptr), " ");
+            mvprintw(ROW0, 4+(*ptr), " ");
+            break;
+        }
+        case 'Z': {
+            mvprintw(ROW9, 4+(*ptr), " ");
+            mvprintw(ROW0, 4+(*ptr), " ");
+            break;
+        }
+        case '&': {
+            mvprintw(ROW12, 4+(*ptr), "*");
+            break;
+        }
+        case '.': {
+            mvprintw(ROW3, 4+(*ptr), " ");
+            mvprintw(ROW8, 4+(*ptr), " ");
+            mvprintw(ROW12, 4+(*ptr), "*");
+            break;
+        }
+        case '-': {
+            mvprintw(ROW11, 4+(*ptr), "*");
+            break;
+        }
+        case '$': {
+            mvprintw(ROW3, 4+(*ptr), " ");
+            mvprintw(ROW8, 4+(*ptr), " ");
+            mvprintw(ROW11, 4+(*ptr), "*");
+            break;
+        }
+        case '*': {
+            mvprintw(ROW4, 4+(*ptr), " ");
+            mvprintw(ROW8, 4+(*ptr), " ");
+            mvprintw(ROW11, 4+(*ptr), "*");
             break;
         }
         case '/': {
-            mvprintw(3, 4+(*ptr)," ");
-            mvprintw(5, 4+(*ptr)," ");
-            break;
-        }
-        case '\'': {
-            mvprintw(13, 4+(*ptr)," ");
-            mvprintw(19, 4+(*ptr)," ");
-            break;
-        }
-        case '@': {
-            mvprintw(11, 4+(*ptr)," ");
-            mvprintw(19, 4+(*ptr)," ");
-            break;
-        }
-        case ':': {
-            mvprintw(7, 4+(*ptr)," ");
-            mvprintw(19, 4+(*ptr)," ");
-            break;
-        }
-        case '?': {
-            mvprintw(3, 4+(*ptr)," ");
-            mvprintw(17, 4+(*ptr)," ");
-            mvprintw(19, 4+(*ptr)," ");
-            break;
-        }
-        case '_': {
-            mvprintw(3, 4+(*ptr)," ");
-            mvprintw(13, 4+(*ptr)," ");
-            mvprintw(19, 4+(*ptr)," ");
-            break;
-        }
-        case '%': {
-            mvprintw(3, 4+(*ptr)," ");
-            mvprintw(11, 4+(*ptr)," ");
-            mvprintw(19, 4+(*ptr)," ");
+            mvprintw(ROW1, 4+(*ptr), " ");
+            mvprintw(ROW0, 4+(*ptr), " ");
             break;
         }
         case ',': {
-            mvprintw(3, 4+(*ptr)," ");
-            mvprintw(9, 4+(*ptr)," ");
-            mvprintw(19, 4+(*ptr)," ");
+            mvprintw(ROW3, 4+(*ptr), " ");
+            mvprintw(ROW8, 4+(*ptr), " ");
+            mvprintw(ROW0, 4+(*ptr), " ");
             break;
         }
-        case ' ': {
-            mvprintw(3, 4+(*ptr)," ");
-            mvprintw(7, 4+(*ptr)," ");
-            mvprintw(19, 4+(*ptr)," ");
+        case '%': {
+            mvprintw(ROW4, 4+(*ptr), " ");
+            mvprintw(ROW8, 4+(*ptr), " ");
+            mvprintw(ROW0, 4+(*ptr), " ");
+            break;
+        }
+        case '#': {
+            mvprintw(ROW3, 4+(*ptr), " ");
+            mvprintw(ROW8, 4+(*ptr), " ");
             break;
         }
         default: {
@@ -175,6 +308,8 @@ void draw_ascii(int x, int y, size_t *ptr,  const char ch) {
 }
 
 int main() {
+    size_t card_index = 0;
+    std::string filename = "card";
     int x, y;
     size_t ptr = 0;
     char ch;
@@ -185,30 +320,38 @@ int main() {
     keypad(stdscr, TRUE);
 
     getmaxyx(stdscr, y, x);
-    draw_ascii(x, y);
     while (true) {
-        ch = getch();
-        if (static_cast<int>(ch) == 10) {
-            FILE *fs = fopen("out.ascii", "w+");
-            if (fs == NULL) {
-                endwin();
-                return 1;
-            }
-            char temp[2];
-            for (int i = 0; i < y; ++i) {
-                for (int j = 0; j < x; ++j) {
-                    temp[0] = mvinch(i, j);
+        draw_ascii(x, y);
+        while (true) {
+            std::this_thread::sleep_for(std::chrono::milliseconds(300));
+            ch = getch();
+            if (static_cast<int>(ch) == 10) {
+                FILE *fs = fopen(std::string(filename + std::to_string(card_index) + ".ascii").c_str(), "w+");
+                if (fs == NULL) {
+                    endwin();
+                    return 1;
+                }
+                char temp[2];
+                for (int i = 0; i < y; ++i) {
+                    for (int j = 0; j < x; ++j) {
+                        temp[0] = mvinch(i, j);
+                        fwrite(temp, sizeof(char), 1, fs);
+                    }
+                    temp[0] = '\n';
                     fwrite(temp, sizeof(char), 1, fs);
                 }
-                temp[0] = '\n';
-                fwrite(temp, sizeof(char), 1, fs);
+                fclose(fs);
+                card_index++;
+                break;
             }
-            fclose(fs);
-            break;
+            else if (static_cast<int>(ch) < 0) {
+                break;
+            }
+            draw_ascii(x, y, &ptr, ch);
+            refresh();
         }
-        draw_ascii(x, y, &ptr, ch);
-        refresh();
-        sleep(1);
+        ptr = 0;
+        std::this_thread::sleep_for(std::chrono::seconds(1));
     }
 
     echo();
